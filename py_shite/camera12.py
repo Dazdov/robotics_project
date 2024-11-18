7a95eda95757473e55d51f1d80d4eff6fd1fdad6
Mikmak94 <eliavhaviv8@gmail.com>
2024-11-18 13:17:38 +0200

    code add of camera and voice shite

---
+++ py_shite/camera12.py
@@ -0,0 +1,15 @@
+import cv2
+
+vid = cv2.VideoCapture(0)
+
+while(True):
+    ret, frame = vid.read()
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows() # Write your code here :-)
