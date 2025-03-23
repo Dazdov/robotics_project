0278aa91ad8a22445902448c731c5348af8d576b
Dazdov <145332140+Dazdov@users.noreply.github.com>
2025-03-23 10:39:40 +0200

    Update arduino_idk.ino

--- ard_shite/arduino_idk.ino
+++ ard_shite/arduino_idk.ino
@@ -1,19 +1,15 @@
 void setup() {
   Serial.begin(9600);
-  // Set pins 5 through 10 as outputs.
   for (int i = 5; i < 11; i++) {
     pinMode(i, OUTPUT);
   }
 }
 
-// This function controls the two motors.
-// 'left' and 'right' are speed percentages (0 to 100).
 void speedMotor(int left, int right) {
-  // Right motor control (pins 8, 9, 10)
   if (right >= 0) {
     digitalWrite(8, HIGH);
     digitalWrite(9, LOW);
-    analogWrite(10, right * 2.55);  // 2.55 scales 0-100 to 0-255
+    analogWrite(10, right * 2.55);
   } else {
     right = -right;
     digitalWrite(8, LOW);
@@ -21,7 +17,6 @@ void speedMotor(int left, int right) {
     analogWrite(10, right * 2.55);
   }
 
-  // Left motor control (pins 6, 7, 5)
   if (left >= 0) {
     digitalWrite(6, HIGH);
     digitalWrite(7, LOW);
@@ -34,41 +29,30 @@ void speedMotor(int left, int right) {
   }
 }
 
-// This function stops both motors.
 void stopMotors() {
-  // Stop right motor
   digitalWrite(8, LOW);
   digitalWrite(9, LOW);
   analogWrite(10, 0);
-
-  // Stop left motor
   digitalWrite(6, LOW);
   digitalWrite(7, LOW);
   analogWrite(5, 0);
 }
 
 void loop() {
-  // Check if there is data available on the Serial port
   if (Serial.available() > 0) {
-    // Read the incoming byte
     char command = Serial.read();
-
-    // Based on the command received, call speedMotor() with the desired speeds.
     if (command == '1') {
-      // Soccer ball is centered: move forward
-      speedMotor(27 * 0.9, 20 * 0.9);  // Decreased by 30%
-      delay(100);  // Move for 10 milliseconds
-      stopMotors();  // Stop the motors after 10 milliseconds
+      speedMotor(54, 40);
+      delay(100);
+      stopMotors();
     } else if (command == '2') {
-      // Soccer ball is to the right: turn right
-      speedMotor(27 * 0.9, -20 * 0.9);  // Decreased by 30%
-      delay(100);  // Move for 10 milliseconds
-      stopMotors();  // Stop the motors after 10 milliseconds
+      speedMotor(27, 0);
+      delay(100);
+      stopMotors();
     } else if (command == '3') {
-      // Soccer ball is to the left: turn left
-      speedMotor(-27 * 0.9, 20 * 0.9);  // Decreased by 30%
-      delay(100);  // Move for 10 milliseconds
-      stopMotors();  // Stop the motors after 10 milliseconds
+      speedMotor(0, 20);
+      delay(100);
+      stopMotors();
     }
   }
 }
